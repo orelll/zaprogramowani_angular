c324a5f3b275183edc641cd3d7885f4fbe9b45ea
szymon.orlik <orlikszym@gmail.com>
2021-10-21 20:05:27 +0200

    koniec lekcji 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root-2',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lekcja2';
+  title: string = 'lekcja 4';
+
+  myArray: string[] = ['a', 'b', 'c'];
+
+  constructor() {
+    console.log(this.myArray)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './komponenty/header/header.component';
+import { NewsListComponent } from './komponenty/news-list/news-list.component';
+import { NewsDataComponent } from './komponenty/news-data/news-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NewsListComponent,
+    NewsDataComponent
   ],
   imports: [
     BrowserModule
