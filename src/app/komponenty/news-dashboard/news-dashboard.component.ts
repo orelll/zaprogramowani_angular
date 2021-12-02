7549350ed7fdc764c547f48589d2f3db446ebb76
szymon.orlik <orlikszym@gmail.com>
2021-12-02 18:28:32 +0100

    koniec lekcji 9

--- src/app/komponenty/news-dashboard/news-dashboard.component.ts
+++ src/app/komponenty/news-dashboard/news-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-news-dashboard',
@@ -8,10 +8,14 @@ import { Component, Input, OnInit } from '@angular/core';
 export class NewsDashboardComponent implements OnInit {
 
   @Input() newsName: string = '';
+  @Output() onClick = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleSize(){
+    this.onClick.emit();
+  }
 }
--- src/app/komponenty/news-list/news-list.component.ts
+++ src/app/komponenty/news-list/news-list.component.ts
@@ -8,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class NewsListComponent implements OnInit {
 
   public newsList: string[] = ['test_1', 'test_2', 'test_3', 'test_4','test_1', 'test_2', 'test_3', 'test_4','test_1', 'test_2', 'test_3', 'test_4',];
+  public shouldBeMaximized: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeSize():void {
+    console.log('hello from parent!');
+    this.shouldBeMaximized = false;
+  }
 }
