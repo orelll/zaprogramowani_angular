b42a5a1a9762a368002d8a012d560a2f3c6ed5dd
szymon.orlik <orlikszym@gmail.com>
2021-12-09 20:01:39 +0100

    koniec lekcji 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
+import { NewsService } from './serwisy/news.service';
 
 @Component({
   selector: 'app-root-2',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title: string = 'lekcja 4';
+  public title: string = 'lekcja 4';
 
   myArray: string[] = ['a', 'b', 'c'];
 
-  constructor() {
-    console.log(this.myArray)
+  constructor(private newService: NewsService) {
+    console.log(this.myArray);
+    
+    newService.getNewsData$()
+                            .subscribe(
+                                        (newsData: string) => 
+                                        console.log(`new news selected with name ${newsData}`)
+                                      );
   }
 }
